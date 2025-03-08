0a30d88d74068a79913ea17b3fad82e9fb445b48
AleeAparicio <alejandroaparicio547@gmail.com>
2025-03-08 20:05:57 -0300

    Actualización: agregado de funciones 'agregarAmigo' y 'mostrarAmigoAgregado'. 08/03/2025

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo');
+    if (amigo.value == "") {
+        alert("Por favor, inserte un nombre.");
+        console.log(amigo.value);
+    } else {
+        amigos.push(amigo.value);
+        console.log(amigo.value, amigos);
+        amigo.value = '';
+        mostrarAmigoAgregado();
+    }
+}
+
+function mostrarAmigoAgregado() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''
+    for (let i = 0; i < amigos.length; i++) {
+        const amigoAgregado = document.createElement('li');
+        amigoAgregado.innerHTML = `${amigos[i]}`;
+        listaAmigos.appendChild(amigoAgregado);
+    }
+}
