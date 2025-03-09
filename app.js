1b6959064181a422f9c817b7d1e88ec0b0a2b01f
AleeAparicio <alejandroaparicio547@gmail.com>
2025-03-09 11:40:53 -0300

    Actualizacion de funciones: agregado de las funciones 'sortearAmigo' y 'refrescar'

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+let amigo = document.getElementById('amigo');
+let listaAmigos = document.getElementById('listaAmigos');
+let amigoSorteado = document.getElementById('resultado');
 
 function agregarAmigo() {
-    let amigo = document.getElementById('amigo');
     if (amigo.value == "") {
         alert("Por favor, inserte un nombre.");
         console.log(amigo.value);
@@ -15,11 +17,28 @@ function agregarAmigo() {
 }
 
 function mostrarAmigoAgregado() {
-    let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = ''
     for (let i = 0; i < amigos.length; i++) {
         const amigoAgregado = document.createElement('li');
         amigoAgregado.innerHTML = `${amigos[i]}`;
         listaAmigos.appendChild(amigoAgregado);
     }
+}
+
+function sortearAmigo() {
+    let amigoSeleccionado = Math.floor(Math.random() * amigos.length);
+    if (amigos == '') {
+        alert("No hay amigos para sortear, ingresa al menos un amigo primero.");
+        console.log(amigos);
+    } else {
+        amigoSorteado.innerHTML = `${amigos[amigoSeleccionado]}`;
+        listaAmigos.innerHTML = '';
+    }
+}
+
+function refrescar() {
+    listaAmigos.innerHTML = '';
+    amigos = [];
+    amigo.value = '';
+    amigoSorteado.innerHTML = '';
 }
